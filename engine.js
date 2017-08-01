027a6c8d00b5e6d89730415086b2eed8252a612c
Johnny Eid <eidjohnny2@gmail.com>
2017-08-01 14:50:44 -0400

    update

--- engine.js
+++ engine.js
@@ -1,154 +1,153 @@
 $(document).ready(function () {
-    var gameBoard = new GameBoard([
-        ["1","","6","","","","8","","9"],
-        ["","3","","","","","","7",""],
-        ["7","","","9","","5","","","3"],
-        ["","","1","7","9","4","5","",""],
-        ["","","","5","","8","","",""],
-        ["","","8","1","3","2","9","",""],
-        ["8","","","3","","7","","","6"],
-        ["","6","","","","","","8",""],
-        ["2","","3","","","","1","","7"]
-    ],[
-        ['1','1','1','2','2','2','3','3','3'],
-        ['1','1','1','2','2','2','3','3','3'],
-        ['1','1','1','2','2','2','3','3','3'],
-        ['4','4','4','5','5','5','6','6','6'],
-        ['4','4','4','5','5','5','6','6','6'],
-        ['4','4','4','5','5','5','6','6','6'],
-        ['7','7','7','8','8','8','9','9','9'],
-        ['7','7','7','8','8','8','9','9','9'],
-        ['7','7','7','8','8','8','9','9','9']
-    ] );
+    var sudoku = new Sudoku([
+        ["1", "", "6", "", "", "", "8", "", "9"],
+        ["", "3", "", "", "", "", "", "7", ""],
+        ["7", "", "", "9", "", "5", "", "", "3"],
+        ["", "", "1", "7", "9", "4", "5", "", ""],
+        ["", "", "", "5", "", "8", "", "", ""],
+        ["", "", "8", "1", "3", "2", "9", "", ""],
+        ["8", "", "", "3", "", "7", "", "", "6"],
+        ["", "6", "", "", "", "", "", "8", ""],
+        ["2", "", "3", "", "", "", "1", "", "7"]
+    ], [
+        ['1', '1', '1', '2', '2', '2', '3', '3', '3'],
+        ['1', '1', '1', '2', '2', '2', '3', '3', '3'],
+        ['1', '1', '1', '2', '2', '2', '3', '3', '3'],
+        ['4', '4', '4', '5', '5', '5', '6', '6', '6'],
+        ['4', '4', '4', '5', '5', '5', '6', '6', '6'],
+        ['4', '4', '4', '5', '5', '5', '6', '6', '6'],
+        ['7', '7', '7', '8', '8', '8', '9', '9', '9'],
+        ['7', '7', '7', '8', '8', '8', '9', '9', '9'],
+        ['7', '7', '7', '8', '8', '8', '9', '9', '9']
+    ]);
 
-    gameBoard.init();
+    //sudoku.init();
 
-    $('#next-button').on('click', function(){
-    	gameBoard.next();
+    $('#next-button').on('click', function () {
+        sudoku.next();
     });
 });
 
 
+class Sudoku {
+
+    constructor(board, group) {
+        this.board = new GameBoard(board, group);
+        this.board.init();
+    }
+
+
+    updatePossibilities() {
+
+        for (var y = 0; y < 9; y++) {
+            for (var x = 0; x < 9; x++) {
+                if ($.isArray(this.final_board[y][x])) {
+
+                    var self = this;
+
+                    this.final_board[y][x].forEach(function (number, index) {
+
+                        if (self.inGroup(number, x, y) || self.inRow(number, x, y) || self.inCol(number, x, y)) {
+                            console.log("removing: " + number + " ( " + x + ", " + y + " )");
+                            self.final_board[y][x].splice(index, 1);
+                        } else {
+                            console.log("Keeping: " + number + " ( " + x + ", " + y + " )");
+                        }
+
+                    });
+                }
+            }
+        }
+    } // updatePossibilities()
+
+    next() {
+        //this.updatePossibilities();
+        //this.updateBoard();
+    } // end next()
+
+
+    /**** Game Rules ****/
+
+    inGroup(tn, tx, ty) {
+
+        var target_group = this.group[ty][tx];
+
+        for (var y = 0; y < 9; y++) {
+            for (var x = 0; x < 9; x++) {
+                if (this.group[y][x] == target_group) {
+                    if (this.final_board[y][x] == tn) {
+                        return true;
+                    }
+                }
+            } // end for x
+        } // end for y
+
+        return false;
+    } // end inGroup()
+
+    inRow(tn, tx, ty) {
+
+        for (var x = 0; x < 9; x++) {
+            if (this.final_board[ty][x] == tn) {
+                return true;
+            }
+        }
+
+        return false;
+    } // end inRow()
+
+    inCol(tn, tx, ty) {
+
+        for (var y = 0; y < 9; y++) {
+            if (this.final_board[y][tx] == tn) {
+                return true;
+            }
+        }
+
+        return false;
+    } // end inRow()
+
+}
 
 
 class GameBoard {
 
     constructor(board, group) {
-        
-        this.base_board 	= board;
-        this.group 			= group;
-        this.final_board 	= [];
-        this.start_board 	= true;
-        
-        for (var y = 0; y < 9; y++){
-    		this.final_board[y] = [];    	
-	        
-	        for (var x = 0; x < 9; x++){
-	        	if ($.isNumeric(this.base_board[y][x])){
-	    			//this.final_board[y][x] = [this.base_board[y][x]];
-	    			this.final_board[y][x] = this.base_board[y][x];
-	    		} else {
-	    			this.final_board[y][x] = [1,2,3,4,5,6,7,8,9];    	
-	        	}
-	        }	
-        }
+
+        this.starting_board = board;
+        this.group = group;
+        this.current_board = [];
     }
 
-    init(){
+    // draw the initial board
+    init() {
 
-    	this.drawBoard();
-    } // end init()
-
-    updatePossibilities(){
-   	
-    	for (var y = 0; y < 9; y++){
-    		for (var x = 0; x < 9; x++){
-	        	if ($.isArray(this.final_board[y][x])){
-                    
-                    var self = this;
-
-	        		this.final_board[y][x].forEach(function(number, index){
-	        		
-                    	if (self.inGroup(number, x, y) || self.inRow(number, x, y) || self.inCol(number, x, y)){
-		        			console.log("removing: " + number + " ( " + x + ", " + y + " )");
-							self.final_board[y][x].splice(index,1);
-	        			} else {
-		        			console.log("Keeping: " + number + " ( " + x + ", " + y + " )");
-	        			}
-	        		
-	        		});
-    			}
-	        }	
-        }
-    } // updatePossibilities()
-
-    inGroup(tn, tx, ty){
-
-    	var target_group = this.group[ty][tx];
-
-    	for (var y = 0; y < 9; y++) {
-        	for (var x = 0; x < 9; x++) {
-        		if (this.group[y][x] == target_group){
-        			if (this.final_board[y][x] == tn){
-        				return true;
-        			}
-        		}
-			} // end for x
-		} // end for y
-
-		return false;
-    } // end inGroup()
-
-
-    inRow(tn, tx, ty){
-    	
-    	for (var x = 0; x < 9; x++) {
-        	if (this.final_board[ty][x] == tn){
-        		return true;
-        	}
-    	}
-
-    	return false;
-    } // end inRow()
-
-    inCol(tn, tx, ty){
-		
-		for (var y = 0; y < 9; y++) {
-        	if (this.final_board[y][tx] == tn){
-        		return true;
-        	}
-    	}
-
-    	return false;
-    } // end inRow()
-    
-
-    next(){
-    	this.updatePossibilities();
-    	this.drawBoard();
-    } // end next()
-
-
-    drawBoard(){
         for (var y = 0; y < 9; y++) {
             for (var x = 0; x < 9; x++) {
 
-               	var box = '.row_' + (y + 1) + ' .col_' + (x + 1);
-                
+                // selector for a box(game piece) on the board
+                var selector = '.row_' + (y + 1) + ' .col_' + (x + 1);
+
                 // add starting values to grid
-                if (this.final_board[y][x].length == 1){
-                	
-                	$(box).html(this.final_board[y][x]);
-                }
+                $(selector).html(this.starting_board[y][x]);
 
-                $(box).addClass(this.group[y][x] % 2 == 0? 'even' : 'odd');
-
-                if (!this.start_board){
-	                $(box).addClass('new-number');
-                }
+                // add game piece group class
+                $(selector).addClass(this.group[y][x] % 2 === 0 ? 'even' : 'odd')
+                    .addClass('starting-value');
             }
         }
 
-        this.start_board = false;
+    }
+
+    setGamePiece(value, x, y) {
+        var selector = '.row_' + (y + 1) + ' .col_' + (x + 1);
+
+        if (this.final_board[y][x].length === 1) {
+            $(selector).html(this.final_board[y][x]);
+        }
+    }
+
+    getCurrentBoard(){
+        return this.current_board;
     }
 }
